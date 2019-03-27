1d669d8263841a70979c0b857a73f9f4d8646045
Michael <michael.pagano@mail.mcgill.ca>
2019-03-26 20:45:34 -0400

    fixed the sed command

--- bash_scripts/box_swap.sh
+++ bash_scripts/box_swap.sh
@@ -1,22 +1,25 @@
 #You must set the initial seed in PARAMS.H to be SEED1 in order for this to work properly
 
-SEED1=10
-SEED2=90
+SEED1=20
+SEED2=80
 
 TOTAL_Z=2
 Z_START=009.00
 Z_END=009.20
-#Z_STEP=
+#Z_STEP=-0.2
 
+COUNTER=1
 ###########################################################################
 #PART1 - RUN ./drive_zscroll_noTs and ./delta_T for both unmodified seeds #
 ########################################################################### 
 #This first part is responsible for running 21cmFAST (without any swap) for each seed and place them in appropriate locations so that we can later move around their corresponding xH boxes
 for i in $SEED1 $SEED2
 do
+	
         #run 21cmFAST for seed1
         ./drive_zscroll_noTs
-
+	echo $21cmFAST is done, now running delta_T
+	
         #run delta_T
         CTR=0
         for j in $Z_START $Z_END
@@ -24,23 +27,34 @@ do
 		CTR=$((CTR+1))
                 #find xH box corresponding to the jth redshift
                 fname=`ls /users/michael/documents/msi-c/21cmfast/boxes/xH* | head -$CTR | tail -1`
-                ./delta_T $j  $fname
-                
+        	echo starting with xH box: $fname
+		  
+		./delta_T $j  $fname
         done
 
         #lets make a folder to put the seed1 boxes and put all of them there
 
+	echo Making a directory for seed $i
         mkdir /users/michael/documents/msi-c/21cmfast/boxes/boxes_for_seed$i
         cp /users/michael/documents/msi-c/21cmfast/boxes/*Mpc /users/michael/documents/msi-c/21cmfast/boxes/boxes_for_seed$i
 	
+	echo copied files
 	#tidy up the folder in preparation for the nextrun
 	rm /users/michael/documents/msi-c/21cmfast/boxes/*Mpc
-	
-        #lets re-write params.h for the next seed
-        sed -i -r 's/#define RANDOM_SEED (long) ($SEED1)/#define RANDOM_SEED (long) ($SEED2)/' /Users/michael/Documents/MSI-C/21cmFAST/Parameter_files/INIT_PARAMS.H
+	echo removing folder
+        
+	#lets re-write params.h for the next seed
+        if [ COUNTER==1 ]
+	then
+		sed -i -r 's/#define RANDOM_SEED (long) ('"$SEED1"')/#define RANDOM_SEED (long) ('"$SEED2"')/' /Users/michael/Documents/MSI-C/21cmFAST/Parameter_files/INIT_PARAMS.H
 
-        #remake the folder
-        make
+	
+		#remake the folder
+		make
+		echo remade the folder
+		COUNTER=$((COUNTER+1))
+	fi
+
 done
 
 #########################################################################
@@ -52,9 +66,11 @@ done
 
 #SEED1 with xH of SEED2
 
+$echo copying seed1 into boxes
 #copy contents of seed1 box into /boxes
 cp /users/michael/documents/msi-c/21cmfast/boxes/boxes_for_seed$SEED1/*Mpc /users/michael/documents/msi-c/21cmfast/boxes/
 
+echo hey hey
 #remove the xH files
 CTR=0
 for j in $Z_START $Z_END
@@ -64,6 +80,8 @@ do
 	fname=`ls /users/michael/documents/msi-c/21cmfast/boxes/xH* | head -$CTR | tail -1`
 	rm $fname
 done
+
+echo hey hey 2.0
 
 #pull the xH files from SEED2 folder into /boxes folder
 CTR=0
@@ -78,6 +96,7 @@ done
 
 #rename the files
 
+echo hey hey 3.0
 CTR=0
 for j in $Z_START $Z_END
 do
@@ -93,6 +112,7 @@ do
 	mv /users/michael/Documents/MSI-C/21cmFAST/Boxes/"$filename" /users/michael/Documents/MSI-C/21cmFAST/Boxes/"$bilename"
 done
 
+echo hey hey 4.0
 
 #run ./delta_T once more
 CTR=0
